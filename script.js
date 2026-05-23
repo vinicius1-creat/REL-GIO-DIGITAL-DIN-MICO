1657c9f1c1339a1d09181ef6461e202290b694f7
progamadorvinicius <vinicius.brow244@gmail.com>
2026-05-23 02:36:37 +0100

    finalizado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const relogion = setInterval(function time() {
+const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
